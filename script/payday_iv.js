1f953249dead821d210a71311af9fcd0cb0a21b6
Phoebe Dao <goibeodangyeu@gmail.com>
2021-12-13 14:40:03 +0700

    data for payday gini

--- script/payday_iv.js
+++ script/payday_iv.js
@@ -2,69 +2,74 @@ var chartDom_iv = document.getElementById('payday_iv');
 var myChart_iv = echarts.init(chartDom_iv);
 var iv_option;
 
+$.get('https://monex-p.github.io/dashboard_prototype/data/payday_iv_data.json', function (my_data) {
+    my_keys = Object.keys(my_data)
 
-iv_option = {
-  title: {
-    text: 'Information Value',
-    textStyle: { color: '#ffffff' }
-  },
-  tooltip: {
-    trigger: 'axis',
-    axisPointer: {
-      type: 'shadow'
+    my_list = []
+    for (var i = 0; i < my_keys.length; i++) {
+        val = my_data[my_keys[i]]['IV']
+        my_sum = Object.values(val).reduce((a, b) => a + b, 0)
+        my_list.push(my_sum)
     }
-  },
+    original_list = my_list.slice();
+    my_list = my_list.sort(function (a, b) {
+        return a-b
+    });
 
-  grid: {
-    left: '3%',
-    right: '4%',
-    bottom: '3%',
-    containLabel: true
-  },
-  xAxis: {
-    type: 'value',
-    axisPointer: 'false',
-    axisLabel: { color: '#ffffff' }
-  },
-  yAxis: {
-    type: 'category',
-    axisTick: 'false',
-    axisLabel: { color: '#ffffff' },
-    // axisPointer:'false',
-    data: [
-      'Origination Weekend',
-      'Origination Hour',
-      'Income',
-      'latitude',
-      'longitude',
-      'Loan 2 Income',
-      'Amount Due',
-      'z_isMadeUpEmail',
-      'Income Frequency',
-      'Loan Term (days)',
-      'Living Arrangement Own',
-      'Communication Preference',
-      'Income Type',
-      'ZIP bad rate',
-      'New Customer',
-      'Income Duration (M)',
-      'Customer Duration (Months)'
-    ]
-  },
-  series: [
-    {
-      name: 'IV',
-      type: 'bar',
-      data: [
-        0.0001596505122738483, 0.0003644931526653962, 0.001326657205182454,
-        0.00133898534047767, 0.006740303175958224, 0.014099570852576506,
-        0.024575599841363423, 0.030717506880877224, 0.03269484792882181,
-        0.035374535755743446, 0.03862818965261583, 0.04401102346874138,
-        0.07024595750220049, 0.07384951630610528, 0.0960557422494557,
-        0.12179762011600537, 0.5040887976916109
-      ]
+    // Get indices of values from original list and rearrange keys
+    sorted_key = []
+    for (i = 0; i < my_list.length; i++) {
+        ind = original_list.indexOf(my_list[i])
+        sorted_key.push(my_keys[ind])
     }
-  ]
-};
+
+    iv_option = {
+        title: {
+            text: 'Information Value',
+            textStyle: { color: '#ffffff' }
+        },
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+                type: 'shadow'
+            }
+        },
+
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        xAxis: {
+            type: 'value',
+            axisPointer: 'false',
+            axisLabel: { color: '#ffffff' },
+            splitLine: {
+                lineStyle: {
+                    color: '#36344E'
+                },
+                show: true
+            },
+        },
+        yAxis: {
+            type: 'category',
+            axisTick: 'false',
+            axisLabel: { color: '#ffffff' },
+            
+            // axisPointer:'false',
+            data: sorted_key
+        },
+        series: [
+            {
+                name: 'IV',
+                type: 'bar',
+                data: my_list
+            }
+        ]
+    };
+
+    myChart_iv.setOption(iv_option);
+});
 
 iv_option && myChart_iv.setOption(iv_option);
