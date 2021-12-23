4b5646a150c5fba531b669b3c2a8f5bfde4fa0ca
ntmy99 <ntmy99.s@gmail.com>
2021-12-23 16:37:12 +0700

    dashboard My updated

--- script/ks_graph.js
+++ script/ks_graph.js
@@ -3,6 +3,9 @@ var myChart_ks = echarts.init(chartDom_ks);
 var option_ks;
 
 
+$.get('https://ntmy99.github.io/db_data.io/ks_bad.json', function (baddata) {
+    $.get('https://ntmy99.github.io/db_data.io/ks_good.json', function (good) {
+        var bad_data = baddata
 option_ks = {
   title: {
     text: 'Kolmogrov Smirnov',
@@ -24,39 +27,8 @@ tooltip: {
     },
 },
   xAxis: {
-    data: ['0.16',
-        '0.18',
-        '0.2',
-        '0.21',
-        '0.23',
-        '0.25',
-        '0.26',
-        '0.28',
-        '0.3',
-        '0.31',
-        '0.33',
-        '0.35',
-        '0.37',
-        '0.38',
-        '0.4',
-        '0.42',
-        '0.43',
-        '0.45',
-        '0.47',
-        '0.48',
-        '0.5',
-        '0.52',
-        '0.54',
-        '0.55',
-        '0.57',
-        '0.59',
-        '0.6',
-        '0.62',
-        '0.64',
-        '0.65',
-        '0.67',
-        '0.68'
-      ],
+    data: bad_data[0],
+    
     axisLabel: { color: '#ffffff' },
     splitLine: {
       show: false
@@ -75,11 +47,7 @@ tooltip: {
       name: 'Good',
       type: 'bar',
       color: '#6495ED',
-      data: [
-        5, 27, 163, 456, 1463, 1420, 1962, 1250, 1875, 1720, 993, 675, 340, 432,
-        306, 262, 171, 108, 130, 124, 147, 126, 111, 149, 192, 142, 107, 78, 75,
-        9, 1, 0
-      ],
+      data: bad_data[1],
       emphasis: {
         focus: 'series'
       },
@@ -91,11 +59,7 @@ tooltip: {
       name: 'Bad',
       type: 'bar',
       color: '#FFA500',
-      data: [
-        1, 1, 20, 78, 241, 347, 414, 457, 786, 521, 513, 312, 272, 403, 418,
-        194, 194, 120, 140, 122, 154, 175, 193, 223, 324, 318, 190, 238, 185,
-        85, 8, 0
-      ],
+      data: bad_data[1],
       emphasis: {
         focus: 'series'
       },
@@ -109,5 +73,8 @@ tooltip: {
     return idx * 5;
   }
 };
-
+myChart_ks.setOption(option_ks);
+    });
+});
 option_ks && myChart_ks.setOption(option_ks);
+    
---
+++ script/scatter_gb.js
@@ -0,0 +1,58 @@
+var chartDom_scattergb = document.getElementById('scattergb');
+var myChart_scattergb = echarts.init(chartDom_scattergb);
+var option_scattergb;
+$.get('https://ntmy99.github.io/db_data.io/scatter_.json', function (data) {
+
+var scatter_gb = data
+option_scattergb = {
+  title: {
+    text: 'Good Bad Scatter',
+    left: 'center',
+    textStyle: { color: '#ffffff' }
+  },
+  grid: {
+    left: '3%',
+    right: '4%',
+    bottom: '3%',
+    containLabel: true
+  },
+  legend: {
+    top: '5%',
+    left: 'center',
+    textStyle:{color: '#ffffff'}
+  },
+  xAxis: [
+    {  splitLine: {
+      show: false
+    },
+      axisLabel: { color: '#ffffff' }
+    }
+  ],
+  yAxis: [
+    { splitLine: {
+      show: false
+    },
+      axisLabel: {
+        color: '#ffffff'
+      }
+    }
+  ],
+  series: [
+    {
+        name: "Good",
+        type: 'scatter',
+        data: scatter_gb[0],
+        symbolSize: 10,
+        color: "#ffcc33"
+    },
+    {   name: "Bad",
+        type: 'scatter',
+        data: scatter_gb[1],
+        symbolSize: 10,
+        color: "#e53315"
+    }
+  ]
+};
+myChart_scattergb.setOption(option_scattergb);
+});
+option_scattergb && myChart_scattergb.setOption(option_scattergb);
